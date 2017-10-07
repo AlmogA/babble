a1e4c6434e4f8fdcb6f2b76c61bfeba0e59c177b
AlmogA <almogas78@gmail.com>
2017-10-07 12:04:06 +0300

    g

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,116 @@
 "use strict";
 
+window.Babble = {currentMessage:"", userInfo: {name:"", email:""}};
+Babble.counter = 0;
+Babble.tabindex = 1;
 
+if(!localStorage.getItem('babble')) {
+ 
+    // display modal
+    document.getElementById("myModal").style.display = "block";
+    localStorage.setItem('babble', JSON.stringify({currentMessage:"", userInfo: {name:"", email:""}})); 
+} else {
+    // update Babble var and textarea with currentMessage
+    setBabble();
+    poll();
+    statusPoll();
+    login();
+}
+
+document.querySelector(".register").addEventListener('click', function(e) {
+    e.preventDefault();
+    Babble.register ({name: document.getElementById("id-name").value,
+                      email: document.getElementById("id-email").value});
+    poll();
+    statusPoll();
+    login();
+});
+
+document.querySelector(".anonymous").addEventListener('click', function(e) {
+    e.preventDefault();
+    Babble.register ({name: "", email:""});
+    poll();
+    statusPoll();
+    login();
+});
+
+function setBabble() {
+    var BabbleLocal = JSON.parse(localStorage.getItem('babble'));
+    Babble.currentMessage = BabbleLocal.currentMessage;
+    Babble.userInfo = BabbleLocal.userInfo;
+    // expand textarea height if needed
+    document.querySelector(".js-growable textarea").value = Babble.currentMessage;
+    document.querySelector(".js-growable span").textContent = Babble.currentMessage;
+}
+
+// text area
+makeGrowable(document.querySelector('.js-growable'));
+function makeGrowable(container) {
+    var area = container.querySelector('textarea');
+    var clone = container.querySelector('span');
+    area.addEventListener('input', function(e) {
+        clone.textContent = area.value;
+        // update localStorage current message
+        Babble.currentMessage = area.value;
+        localStorage.setItem('babble', JSON.stringify({currentMessage: Babble.currentMessage,
+                                                       userInfo: Babble.userInfo})); 
+    });
+}
+
+Babble.register = function (userInfo) {
+    Babble.currentMessage = "";
+    Babble.userInfo = userInfo;
+    localStorage.setItem('babble', JSON.stringify({currentMessage: Babble.currentMessage,
+                                                   userInfo: Babble.userInfo}));
+    document.getElementById("myModal").style.display = "none";
+}
+
+function login() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:9000/login");
+    xhr.send(); 
+}
+
+window.addEventListener("unload", function() {
+    navigator.sendBeacon("http://localhost:9000/logout");
+}, false); 
+
+Babble.postMessage = function (message, callback) {
+    /*
+    request({
+        method: "POST",
+        action: "http://localhost:9000/messages",
+        data: message
+    }).then(function(result) {
+        callback(JSON.parse(result));
+    }); */
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:9000/messages");
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.addEventListener('load', function(e) {
+         callback(JSON.parse(e.target.responseText));
+    });
+    xhr.send(JSON.stringify(message));
+}
+
+function emptyTextarea () {
+    document.querySelector(".js-growable textarea").value = "";
+    document.querySelector(".js-growable span").textContent = "";
+    Babble.currentMessage = "";
+    localStorage.setItem('babble', JSON.stringify({currentMessage: Babble.currentMessage,
+                                                    userInfo: Babble.userInfo})); 
+}
+
+document.getElementById("messageForm").addEventListener('submit', function(e) {
+    e.preventDefault();
+    var data = {
+        name: Babble.userInfo.name,
+        email: Babble.userInfo.email,
+        message: document.querySelector("textarea").value,
+        timestamp: Date.now()
+    };
+    Babble.postMessage(data, emptyTextarea);
+});
 
 document.getElementById("messageForm").addEventListener("keyup", function(event) {
     event.preventDefault();
